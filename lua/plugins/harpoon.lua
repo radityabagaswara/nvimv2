110e47e014a9cea45717b8a9f7a261992f2539af
Raditya Bagaswara <radityabagaswaraa@gmail.com>
2025-03-13 13:09:12 +0700

    added harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          ";H",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          ";h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+      return keys
+    end,
+  },
+}
