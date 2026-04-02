9119e2696336131e5c9e69de0689d7c2e14f5fe0
Raditya Bagaswara <radityabagaswaraa@gmail.com>
2026-04-02 14:56:10 +0700

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,34 @@ return {
           filetypes = { "java" },
           mason = false,
         },
+        kotlin_language_server = {
+          mason = false,
+          -- Increase RAM allocation. Multi-module Maven parsing is heavy.
+          cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/kotlin-lsp"), "-J-Xmx2G", "--stdio" },
+          filetypes = { "kotlin" },
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            -- 1. Prioritize .git or mvnw to guarantee we hit the TRUE parent root
+            local root = util.root_pattern(".git", "mvnw")(fname)
+
+            -- 2. Fallback: If not using git/mvnw, find the top-most pom.xml
+            -- (This is a simplified fallback, but .git is usually the golden ticket)
+            if not root then
+              root = util.root_pattern("pom.xml")(fname)
+            end
+
+            return root
+          end,
+          settings = {
+            kotlin = {
+              indexing = {
+                enabled = true,
+              },
+            },
+          },
+        },
       },
+
       -- Force it to recognize .cs files immediately
       setup = {
         csharp_ls = function(_, opts)
@@ -27,7 +54,13 @@ return {
     keys = {
       { "<leader>zz", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
       { "K", false },
-      { "<leader>k", function() return vim.lsp.buf.hover() end, desc = "Hover" },
+      {
+        "<leader>k",
+        function()
+          return vim.lsp.buf.hover()
+        end,
+        desc = "Hover",
+      },
     },
   },
 }
